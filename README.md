138e4d7363d6c92f72972e06bf1b534867a78b28
Tiryaki <Chen.Tiryaki@kcc.com>
2020-09-22 23:34:44 -0400

    refreshed extract and files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # project_travel_plan
+
+## Jupyter Notebook
+create the following files for data extraction:
+- config.py
+code in file:
+SODAPY_APPTOKEN= "API KEY HERE"
+- config_db.py
+password_db = "POSGRES DATABASE PASSWORD HERE"
+
+## Posgres SQL Database
+- create a new database called travel_data
+- run the script from location the database in Posgres (PG Admin was used for this project)
+
+## Application
+- go to directory where app.py exist, and run using Python
+- launch localhost to call the index.html and visualize charts
--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from config_db import password_db
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, redirect
 import pandas as pd
 import json
 
@@ -26,13 +26,9 @@ Base.prepare(engine, reflect = True)
 # Save references to each table
 travel_details = Base.classes.travel_data_details()
 
+# verify table content
 Travel_Details = Base.classes.travel_data_details
 Travel_Details.__dict__
-
-
-
-
-# no_travel
 
 # Setup Flask
 app = Flask(__name__)
@@ -41,20 +37,21 @@ app = Flask(__name__)
 # main page
 @app.route("/")
 def home():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/notravel<br/>"
-        f"/api/v1.0/geomap<br/>"
-        f"/api/v1.0/test<br/>"
-        f"/api/v1.0/test2<br/>"
-        f"/api/v1.0/'start_date'<br/>"
-        f"/api/v1.0/'start_date'/'end_date'<br/>"
-        )
+    # """List all available api routes."""
+    # return (
+    #     f"Available Routes:<br/>"
+    #     f"/api/v1.0/notravel<br/>" #to be retired
+    #     f"/api/v1.0/geomap<br/>" #to be retired
+    #     f"/api/v1.0/travel_trips<br/>"
+    #     f"/api/v1.0/travel_summary<br/>"
+
+    #     )
+
+
 # # this is to call the index.html - place it when html is ready
 # def home():
 #     mars_data = mongo.db.collection.find_one()
-#     return render_template('index.html',mars_info = mars_data)
+    return render_template('index.html')
 
 
 # precipitation
@@ -97,10 +94,10 @@ def geomap():
     return jsonify(geomap_travel)
 
 
-@app.route("/api/v1.0/test")
-def test():
+@app.route("/api/v1.0/travel_trips")
+def traveltrips():
     connection = engine.connect()
-    test_travel_df = pd.read_sql("""select year, month, month_abbr, month_year, state, 
+    travel_trips_df = pd.read_sql("""select year, month, month_abbr, month_year, state, 
         round(avg(pop_stay_at_home),0) as no_travel_pop, round(avg(pop_not_stay_at_home),0) as travel_pop, 
         round(avg(trips),0) as total_trips, round(avg(trips_1),0) as trips_1, round(avg(trips_1_3),0) as trips_1_3,
         round(avg(trips_3_5),0) as trips_3_5, round(avg(trips_5_10),0) as trips_5_10, round(avg(trips_10_25),0) as trips_10_25,
@@ -109,95 +106,44 @@ def test():
         round(avg(trips_500),0) as trips_500 from travel_data_details 
         GROUP BY state, month_year, year, month, month_abbr
         order by state, year, month, month_year, month_abbr """, connection)
-    test_travel_json1 = json.dumps(test_travel_df.to_dict('records'))
+
+    travel_trips_df['no_travel_pop'] = travel_trips_df['no_travel_pop'].astype('int')
+    travel_trips_df['travel_pop'] = travel_trips_df['travel_pop'].astype('int')
+    travel_trips_df['total_trips'] = travel_trips_df['total_trips'].astype('int')
+    travel_trips_df['trips_1'] = travel_trips_df['trips_1'].astype('int')
+    travel_trips_df['trips_1_3'] = travel_trips_df['trips_1_3'].astype('int')
+    travel_trips_df['trips_3_5'] = travel_trips_df['trips_3_5'].astype('int')
+    travel_trips_df['trips_5_10'] = travel_trips_df['trips_5_10'].astype('int')
+    travel_trips_df['trips_10_25'] = travel_trips_df['trips_10_25'].astype('int')
+    travel_trips_df['trips_25_50'] = travel_trips_df['trips_25_50'].astype('int')
+    travel_trips_df['trips_50_100'] = travel_trips_df['trips_50_100'].astype('int')
+    travel_trips_df['trips_100_250'] = travel_trips_df['trips_100_250'].astype('int')
+    travel_trips_df['trips_250_500'] = travel_trips_df['trips_250_500'].astype('int')
+    travel_trips_df['trips_500'] = travel_trips_df['trips_500'].astype('int')
+
+
+    travel_detail_json = json.dumps(travel_trips_df.to_dict('records'))
     # session.close()
 
-    return test_travel_json1
+    return travel_detail_json
 
-@app.route("/api/v1.0/test2")
-def test2():
+@app.route("/api/v1.0/travel_summary")
+def travelsummary():
     connection = engine.connect()
-    test_travel_df = pd.read_sql('select year, month, state, round(avg(pop_stay_at_home),0) as no_travel_pop from travel_data_details GROUP BY year, month, state', connection)
-    test_travel_json = test_travel_df.to_json()
+    travel_summary_df = pd.read_sql("""select year, month, month_abbr, month_year, state, latitude, longitude,
+        round(avg(pop_stay_at_home),0) as no_travel_pop, round(avg(pop_not_stay_at_home),0) as travel_pop from travel_data_details 
+        GROUP BY state, month_year, year, month, month_abbr, latitude, longitude
+        order by state, year, month, month_year, month_abbr """, connection)
+
+    travel_summary_df['no_travel_pop'] = travel_summary_df['no_travel_pop'].astype('int')
+    travel_summary_df['travel_pop'] = travel_summary_df['travel_pop'].astype('int')
+
+    travel_summary_json = json.dumps(travel_summary_df.to_dict('records'))
     # session.close()
 
-    return test_travel_json
+    return travel_summary_json
 
 
-
-
-# # temperature observation
-# @app.route("/api/v1.0/tobs")
-# def tobs():
-#     session=Session(engine)
-
-#     # Calculate the date 1 year ago from the last data point in the database
-#     count_station = session.query(Measurement.station,(func.count(Measurement.station))).\
-#     group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
-    
-#     # unpack tuple
-#     unpack = [station for station,count in count_station]
-    
-#     # get the most active station
-#     most_active_station = unpack[0]
-    
-#     # find the last date available in dataset for the most active station
-#     last_date = session.query(Measurement.date).\
-#     filter(Measurement.station==most_active_station).\
-#     order_by(Measurement.date.desc()).first() 
-
-#     # get date to format and calculate last year
-#     dateparts = last_date[0].split('-')
-#     previousyear = dt.date(int(dateparts[0]),int(dateparts[1]),int(dateparts[2]))- dt.timedelta(days=365)
-
-#     # find temperature list for previous year for the most active station
-#     tobs_data = session.query(Measurement.date,Measurement.tobs).\
-#     filter(Measurement.date > previousyear).\
-#     filter(Measurement.station==most_active_station).\
-#     order_by(Measurement.date.desc()).all()
-
-#     session.close()
-
-#     all_tobs = list(np.ravel(tobs_data))
-
-#     return jsonify(all_tobs)
-
-# # get MIN,AVG,MAX greater and equal to start date
-# @app.route("/api/v1.0/<start_date>")
-# def start(start_date):
-#     """Data displayed as MIN, AVG, MAX temperatures"""
-#     session=Session(engine)
-
-#     # get data based on start date entered
-#     start_data = session.query(func.min(Measurement.tobs),\
-#     func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
-#     filter(Measurement.date>=start_date).all()
-     
-#     session.close()
-
-#     result = list(np.ravel(start_data))
-
-#     return jsonify(result)
-
-
-# # get MIN,AVG,MAX between start date and end date, inclusive
-# @app.route("/api/v1.0/<start_date>/<end_date>")
-# def startend(start_date,end_date):
-#     """Data displayed as MIN, AVG, MAX temperatures for the interval"""
-#     session=Session(engine)
-
-#     # get data based on interval dates entered
-#     start_end_data = session.query(func.min(Measurement.tobs),\
-#     func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
-#     filter(Measurement.date>=start_date).\
-#     filter(Measurement.date<=end_date).all()
-     
-#     session.close()
-
-#     result1 = list(np.ravel(start_end_data))
-
-#     return jsonify(result1)
-   
 
 if __name__ == '__main__':
     app.run(debug=True)
